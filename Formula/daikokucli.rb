20ffe9a50a2423af89eb669d8e4aa88201446681
Etienne ANNE <etienne.anne@icloud.com>
2024-09-02 08:57:20 +0200

    add daikoku cli Formula

---
+++ Formula/daikokucli.rb
@@ -0,0 +1,15 @@
+class Wasmo < Formula
+  desc "Daikoku CLI"
+  homepage "https://github.com/MAIF/daikoku/tree/master/cli"
+  license any_of: ["MIT", "0BSD"]
+  url "https://github.com/MAIF/daikoku/archive/refs/tags/cli-1.0.0.tar.gz"
+  sha256 "8fefed875fa48e9fa2de1ee6d864e9baa93ad2b557f42ea35bad1343034adde8"
+  version "1.0.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--manifest-path=cli/Cargo.toml", "--release", "--bin", "daikokucli"
+    bin.install "cli/target/release/daikokucli"
+  end
+end
