581037cb9432b613bf07feea0b1b4bd14afa91fb
Etienne ANNE <etienne.anne@icloud.com>
2024-12-04 09:58:39 +0100

    Update daikoku.rb

--- Formula/daikoku.rb
+++ Formula/daikoku.rb
@@ -2,9 +2,9 @@ class Daikoku < Formula
   desc "Daikoku CLI"
   homepage "https://github.com/MAIF/daikoku/tree/master/cli"
   license any_of: ["MIT", "0BSD"]
-  url "https://github.com/MAIF/daikoku/archive/refs/tags/cli-1.0.0.tar.gz"
-  sha256 "8fefed875fa48e9fa2de1ee6d864e9baa93ad2b557f42ea35bad1343034adde8"
-  version "1.0.0"
+  url "https://github.com/MAIF/daikoku/archive/refs/tags/cli-1.1.0.tar.gz"
+  sha256 "0e6925d7e5b055535fc616f2c0a5284b7208a73cfc63f95bd7edb0d593e82940"
+  version "1.1.0"
 
   depends_on "rust" => :build
 
