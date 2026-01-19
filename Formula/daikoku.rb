a0ae3ab4f7fefd0e72a52daadf6cf4144cc8549e
Quentin Aubert <quentino.vega@gmail.com>
2026-01-19 14:02:32 +0100

    Update daikoku formula to get just binary

--- Formula/daikoku.rb
+++ Formula/daikoku.rb
@@ -2,14 +2,17 @@ class Daikoku < Formula
   desc "Daikoku CLI"
   homepage "https://github.com/MAIF/daikoku/tree/master/cli"
   license any_of: ["MIT", "0BSD"]
-  url "https://github.com/MAIF/daikoku/archive/refs/tags/cli-1.4.0.tar.gz"
-  sha256 "84bb5fac4c6321c70386fc43c055d0bde58773098955d09bafe6a07ed11d4089"
   version "1.4.0"
 
-  depends_on "rust" => :build
+  if Hardware::CPU.arm?
+    url "https://github.com/MAIF/daikoku/releases/download/cli-1.4.0/daikoku-darwin-arm64"
+    sha256 "15c740b318049d0efcddef8c65f89e2c068e96d71ac66bbb22e6876032779c51"
+  else
+    url "https://github.com/MAIF/daikoku/releases/download/cli-1.4.0/daikoku-darwin-amd64"
+    sha256 "9427b4fdfae2f5f65ebc73d8a502e6da0d86cb2b52f1538e20e365364d8e2479"
+  end
 
   def install
-    system "cargo", "build", "--manifest-path=cli/Cargo.toml", "--release", "--bin", "daikoku"
-    bin.install "cli/target/release/daikoku"
+    bin.install "daikoku-darwin-#{Hardware::CPU.arm? ? 'arm64' : 'amd64'}" => "daikoku"
   end
 end
