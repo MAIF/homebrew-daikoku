cbff76c7aa67b15d6f6d1878b4c999bee766e3ed
Quentin Aubert <quentino.vega@gmail.com>
2026-01-19 13:42:50 +0100

    Update version for 1.4.0

--- Formula/daikoku.rb
+++ Formula/daikoku.rb
@@ -2,9 +2,9 @@ class Daikoku < Formula
   desc "Daikoku CLI"
   homepage "https://github.com/MAIF/daikoku/tree/master/cli"
   license any_of: ["MIT", "0BSD"]
-  url "https://github.com/MAIF/daikoku/archive/refs/tags/cli-1.3.0.tar.gz"
-  sha256 "08d03866ee22ff8c9ecb94d4d3bbfcf236e8b56284d85920dfc687e13aa57569"
-  version "1.3.0"
+  url "https://github.com/MAIF/daikoku/archive/refs/tags/cli-1.4.0.tar.gz"
+  sha256 "84bb5fac4c6321c70386fc43c055d0bde58773098955d09bafe6a07ed11d4089"
+  version "1.4.0"
 
   depends_on "rust" => :build
 
